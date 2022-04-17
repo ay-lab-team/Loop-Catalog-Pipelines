c12428cdc2586259daae26dd4409e09ff1097738
Nikhil Rao <nrao@herman-login1.hpc.lji.org>
2022-04-16 21:55:04 -0700

    adding chipline/chip-seq files/folders

---
+++ workflow/scripts/run_chipline.sh
@@ -0,0 +1,63 @@
+#!/bin/bash -ex
+
+#PBS -l nodes=1:ppn=16
+#PBS -l mem=100GB
+#PBS -l walltime=40:00:00
+#PBS -m ae
+#PBS -j oe
+#PBS -V
+
+# Usage:
+# ./workflow/scripts/run_chipline.sh <input fastq folder> <output folder name>
+
+source ~/.bashrc
+hostname
+TMPDIR=/scratch
+cd $PBS_O_WORKDIR
+
+source activate chipline
+PATH=/share/apps/R/3.6.1/bin/:$PATH
+PATH=/mnt/BioAdHoc/Groups/vd-ay/nrao/hichip_database/chipline/software:$PATH
+
+PATH=/mnt/BioAdHoc/Groups/vd-ay/nrao/hichip_database/chipline/software/phantompeakqualtools/:$PATH
+PATH=/share/apps/picard-tools/picard-tools-2.7.1/:$PATH
+PATH=/share/apps/python/python-3.4.6/bin/:$PATH # deeptools
+
+
+#=================
+# main executable script of the ChIP seq pipeline
+#=================
+# developed by - Sourya Bhattacharyya
+# Vijay-AY lab
+# La Jolla Institute for Allergy and Immunology
+#=================
+
+CodeDir="/mnt/BioAdHoc/Groups/vd-ay/nrao/hichip_database/chipline/ChIPLine/"
+CodeExec="$CodeDir/bin/pipeline.sh"
+
+#=================
+# script 1 - when fastq files of paired end read are provided as the input
+#=================
+
+workdir="results/loops/chipline"
+genome="/mnt/BioAdHoc/Groups/vd-ay/Database_HiChIP_eQTL_GWAS/Data/RefGenome/bowtie2_index/hg38/hg38"
+
+# directory with input fastqs
+dirdata=$1
+
+fastqs=()
+for FILE in $dirdata/*.fastq.gz
+do
+    fastqs+=($FILE)
+done
+
+inpfile1=${fastqs[0]}
+inpfile2=${fastqs[1]}
+
+prefix=$2
+
+outdir="$workdir/$prefix"
+mkdir -p $outdir
+
+# Note: no control file (-c parameter) used
+$CodeExec -f $inpfile1 -r $inpfile2 -C "config/chipline/configfile.txt" -n $prefix -g $genome -d $outdir -t 16 -m "16G" -T 0 -q 30 -D 1 -p "hs" -O 1
