31d654d63f308471e3e17390b9274007495b7c2a
Nikhil Rao <nrao@herman-login1.hpc.lji.org>
2022-04-20 13:38:02 -0700

    no more conda environment for chipline script

--- workflow/scripts/run_chipline.sh
+++ workflow/scripts/run_chipline.sh
@@ -20,18 +20,20 @@ hostname
 TMPDIR=/scratch
 cd $PBS_O_WORKDIR
 
-# run bash in script mode
+# run bash in strict mode
 set -euo pipefail
 IFS=$'\n\t'
 
-source activate chipline
+# source activate chipline
 PATH=/share/apps/R/3.6.1/bin/:$PATH
-PATH=/mnt/BioAdHoc/Groups/vd-ay/nrao/hichip_database/chipline/software:$PATH
-PATH=/mnt/BioAdHoc/Groups/vd-ay/nrao/hichip_database/chipline/software/bowtie2/bowtie2-2.4.5:$PATH #bowtie2
+PATH=/mnt/BioAdHoc/Groups/vd-ay/nrao/hichip_database/chipline/software:$PATH # Utilities "bedGraphToBigWig", "bedSort", "bigBedToBed", "hubCheck", "fetchChromSizes", and HOMER
+PATH=/mnt/BioAdHoc/Groups/vd-ay/nrao/hichip_database/chipline/software/bowtie2/bowtie2-2.4.5:$PATH # bowtie2
+PATH=/mnt/BioAdHoc/Groups/vd-ay/nrao/hichip_database/chipline/software/samtools/samtools-1.15.1:$PATH # samtools
 
-PATH=/mnt/BioAdHoc/Groups/vd-ay/nrao/hichip_database/chipline/software/phantompeakqualtools/:$PATH
-PATH=/share/apps/picard-tools/picard-tools-2.7.1/:$PATH
+PATH=/mnt/BioAdHoc/Groups/vd-ay/nrao/hichip_database/chipline/software/phantompeakqualtools/:$PATH # phantompeakqualtools
+PATH=/share/apps/picard-tools/picard-tools-2.7.1/:$PATH # picard
 PATH=/share/apps/python/python-3.4.6/bin/:$PATH # deeptools
+PATH=/share/apps/python/python-2.7.13/bin/:$PATH # macs2
 
 #=================
 # main executable script of the ChIP seq pipeline
