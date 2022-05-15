3cc23d6ddaf689b162065d033a38e8f0cf7529a7
Kyra Fetter <kfetter@herman-login1.hpc.lji.org>
2022-05-14 22:07:36 -0700

    rename scripts

--- workflow/scripts/loops/run_fithichip_loopcalling.sh
+++ workflow/scripts/loops/run_fithichip_loopcalling.sh
@@ -31,7 +31,7 @@ cd $PBS_O_WORKDIR
 source workflow/source_paths.sh
 
 # extract the sample information using the PBS ARRAYID
-samplesheet="results/samplesheets/fastq/2022.04.09.16.57.fastq.samplesheet.without_header.tsv"
+samplesheet="results/samplesheets/hicpro/2022.04.09.16.57.hicpro.samplesheet.without_header.tsv"
 sample_info=( $(cat $samplesheet | sed -n "${PBS_ARRAYID}p") )
 sample_name="${sample_info[0]}"
 
@@ -42,18 +42,502 @@ echo "----------"
 echo "sample_name: $sample_name"
 echo
 
-# make the output directory 
-outdir="results/loops/fithichip/$sample_name/"
-mkdir -p $outdir
+# make the output directories
+outdir_L5="results/loops/fithichip/$sample_name/L5/"
+outdir_S5="results/loops/fithichip/$sample_name/S5/"
+outdir_L10="results/loops/fithichip/$sample_name/L10/"
+outdir_S10="results/loops/fithichip/$sample_name/S10/"
+outdir_L25="results/loops/fithichip/$sample_name/L25/"
+outdir_S25="results/loops/fithichip/$sample_name/S25/"
+mkdir -p $outdir_L5 $outdir_S5 $outdir_L10 $outdir_S10 $outdir_L25 $outdir_S25
 
-# run fithichip, use chipseq peaks if avaliable, hichip peaks otherwise
-echo "# running fithichip"
+# identify hicpro validpairs file if avaliable
+echo "# finding hicpro validpairs file"
+validpairs_file="results/hicpro/${sample_name}/hic_results/data/${sample_name}/${sample_name}.allValidPairs"
+if [ -f "$validpairs_file" ]; then
+    echo "validpairs file found and will be used to call loops"
+    pairs_file= $validpairs_file
+else 
+    echo "validpairs file not found"
+    exit 2
+fi
+
+# identify chipseq peaks file if avaliable, hichip peaks file otherwise
+echo "# finding peaks file"
 chipseq_file= # INSERT PATH TO CHIPSEQ FILE HERE
 if [ -f "$chipseq_file" ]; then
     echo "chip-seq peaks found and will be used to call loops"
+    peaks_file= $chipseq_file
 else 
     echo "chip-seq peaks not found. hichip peaks will be used to call loops"
+    peaks_file="results/peaks/fithichip/$sample_name/MACS2_ExtSize/out_macs2_peaks.narrowPeak"
 fi
+
+####################################################################################################################
+
+# generate config file for L, 5kb
+configfile_L5="results/loops/fithichip/$sample_name/configfile_L5"
+touch $configfile_L5
+cat <<EOT >> $configfile_L5
+# File containing the valid pairs from HiCPro pipeline 
+ValidPairs=\${pairs_file}
+
+# File containing the bin intervals (according to a specified bin size)
+# which is an output of HiC-pro pipeline
+# If not provided, this is computed from the parameter 1
+Interval=
+
+# File storing the contact matrix (output of HiC-pro pipeline)
+# should be accompanied with the parameter 2
+# if not specified, computed from the parameter 1
+Matrix=
+
+# Pre-computed locus pair file
+# of the format: 
+# chr1 	start1 	end1 	chr2 	start2 	end2 	contactcounts
+Bed=
+
+# Boolean variable indicating if the reference genome is circular
+# by default, it is 0
+# if the genome is circular, the calculation of genomic distance is slightly different
+CircularGenome=0
+
+# File containing reference ChIP-seq / HiChIP peaks (in .bed format)
+# mandatory parameter
+PeakFile=\${peaks_file}
+
+# Output base directory under which all results will be stored
+OutDir=\${outdir_L5}
+
+#Interaction type - 1: peak to peak 2: peak to non peak 3: peak to all (default) 4: all to all 5: everything from 1 to 4.
+IntType=3
+
+# Size of the bins [default = 5000], in bases, for detecting the interactions.
+BINSIZE=5000
+
+# Lower distance threshold of interaction between two segments
+# (default = 20000 or 20 Kb)
+LowDistThr=20000
+
+# Upper distance threshold of interaction between two segments
+# (default = 2000000 or 2 Mb)
+UppDistThr=2000000
+
+# Applicable only for peak to all output interactions - values: 0 / 1
+# if 1, uses only peak to peak loops for background modeling - corresponds to FitHiChIP(S)
+# if 0, uses both peak to peak and peak to nonpeak loops for background modeling - corresponds to FitHiChIP(L)
+UseP2PBackgrnd=0
+
+# parameter signifying the type of bias vector - values: 1 / 2
+# 1: coverage bias regression	2: ICE bias regression
+BiasType=1
+
+# following parameter, if 1, means that merge filtering (corresponding to either FitHiChIP(L+M) or FitHiChIP(S+M))
+# depending on the background model, would be employed. Otherwise (if 0), no merge filtering is employed. Default: 1
+MergeInt=1
+
+# FDR (q-value) threshold for loop significance
+QVALUE=0.01
+
+# File containing chromomosome size values corresponding to the reference genome.
+ChrSizeFile=/mnt/BioAdHoc/Groups/vd-ay/Database_HiChIP_eQTL_GWAS/Data/RefGenome/chrsize/hg38.chrom.sizes
+
+# prefix string of all the output files (Default = 'FitHiChIP').
+PREFIX=FitHiChIP_L5
+
+# Binary variable 1/0: if 1, overwrites any existing output file. otherwise (0), does not overwrite any output file.
+OverWrite=1
+EOT
+
+#####################################################################################################################
+
+# generate config file for S, 5kb
+configfile_S5="results/loops/fithichip/$sample_name/configfile_S5"
+touch $configfile_S5
+cat <<EOT >> $configfile_S5
+# File containing the valid pairs from HiCPro pipeline 
+ValidPairs=\${pairs_file}
+
+# File containing the bin intervals (according to a specified bin size)
+# which is an output of HiC-pro pipeline
+# If not provided, this is computed from the parameter 1
+Interval=
+
+# File storing the contact matrix (output of HiC-pro pipeline)
+# should be accompanied with the parameter 2
+# if not specified, computed from the parameter 1
+Matrix=
+
+# Pre-computed locus pair file
+# of the format: 
+# chr1 	start1 	end1 	chr2 	start2 	end2 	contactcounts
+Bed=
+
+# Boolean variable indicating if the reference genome is circular
+# by default, it is 0
+# if the genome is circular, the calculation of genomic distance is slightly different
+CircularGenome=0
+
+# File containing reference ChIP-seq / HiChIP peaks (in .bed format)
+# mandatory parameter
+PeakFile=\${peaks_file}
+
+# Output base directory under which all results will be stored
+OutDir=\${outdir_S5}
+
+#Interaction type - 1: peak to peak 2: peak to non peak 3: peak to all (default) 4: all to all 5: everything from 1 to 4.
+IntType=3
+
+# Size of the bins [default = 5000], in bases, for detecting the interactions.
+BINSIZE=5000
+
+# Lower distance threshold of interaction between two segments
+# (default = 20000 or 20 Kb)
+LowDistThr=20000
+
+# Upper distance threshold of interaction between two segments
+# (default = 2000000 or 2 Mb)
+UppDistThr=2000000
+
+# Applicable only for peak to all output interactions - values: 0 / 1
+# if 1, uses only peak to peak loops for background modeling - corresponds to FitHiChIP(S)
+# if 0, uses both peak to peak and peak to nonpeak loops for background modeling - corresponds to FitHiChIP(L)
+UseP2PBackgrnd=1
+
+# parameter signifying the type of bias vector - values: 1 / 2
+# 1: coverage bias regression	2: ICE bias regression
+BiasType=1
+
+# following parameter, if 1, means that merge filtering (corresponding to either FitHiChIP(L+M) or FitHiChIP(S+M))
+# depending on the background model, would be employed. Otherwise (if 0), no merge filtering is employed. Default: 1
+MergeInt=1
+
+# FDR (q-value) threshold for loop significance
+QVALUE=0.01
+
+# File containing chromomosome size values corresponding to the reference genome.
+ChrSizeFile=/mnt/BioAdHoc/Groups/vd-ay/Database_HiChIP_eQTL_GWAS/Data/RefGenome/chrsize/hg38.chrom.sizes
+
+# prefix string of all the output files (Default = 'FitHiChIP').
+PREFIX=FitHiChIP_S5
+
+# Binary variable 1/0: if 1, overwrites any existing output file. otherwise (0), does not overwrite any output file.
+OverWrite=1
+EOT
+
+#####################################################################################################################
+
+# generate config file for L, 10kb
+configfile_L10="results/loops/fithichip/$sample_name/configfile_L10"
+touch $configfile_L10
+cat <<EOT >> $configfile_L10
+# File containing the valid pairs from HiCPro pipeline 
+ValidPairs=\${pairs_file}
+
+# File containing the bin intervals (according to a specified bin size)
+# which is an output of HiC-pro pipeline
+# If not provided, this is computed from the parameter 1
+Interval=
+
+# File storing the contact matrix (output of HiC-pro pipeline)
+# should be accompanied with the parameter 2
+# if not specified, computed from the parameter 1
+Matrix=
+
+# Pre-computed locus pair file
+# of the format: 
+# chr1 	start1 	end1 	chr2 	start2 	end2 	contactcounts
+Bed=
+
+# Boolean variable indicating if the reference genome is circular
+# by default, it is 0
+# if the genome is circular, the calculation of genomic distance is slightly different
+CircularGenome=0
+
+# File containing reference ChIP-seq / HiChIP peaks (in .bed format)
+# mandatory parameter
+PeakFile=\${peaks_file}
+
+# Output base directory under which all results will be stored
+OutDir=\${outdir_L10}
+
+#Interaction type - 1: peak to peak 2: peak to non peak 3: peak to all (default) 4: all to all 5: everything from 1 to 4.
+IntType=3
+
+# Size of the bins [default = 5000], in bases, for detecting the interactions.
+BINSIZE=10000
+
+# Lower distance threshold of interaction between two segments
+# (default = 20000 or 20 Kb)
+LowDistThr=20000
+
+# Upper distance threshold of interaction between two segments
+# (default = 2000000 or 2 Mb)
+UppDistThr=2000000
+
+# Applicable only for peak to all output interactions - values: 0 / 1
+# if 1, uses only peak to peak loops for background modeling - corresponds to FitHiChIP(S)
+# if 0, uses both peak to peak and peak to nonpeak loops for background modeling - corresponds to FitHiChIP(L)
+UseP2PBackgrnd=0
+
+# parameter signifying the type of bias vector - values: 1 / 2
+# 1: coverage bias regression	2: ICE bias regression
+BiasType=1
+
+# following parameter, if 1, means that merge filtering (corresponding to either FitHiChIP(L+M) or FitHiChIP(S+M))
+# depending on the background model, would be employed. Otherwise (if 0), no merge filtering is employed. Default: 1
+MergeInt=1
+
+# FDR (q-value) threshold for loop significance
+QVALUE=0.01
+
+# File containing chromomosome size values corresponding to the reference genome.
+ChrSizeFile=/mnt/BioAdHoc/Groups/vd-ay/Database_HiChIP_eQTL_GWAS/Data/RefGenome/chrsize/hg38.chrom.sizes
+
+# prefix string of all the output files (Default = 'FitHiChIP').
+PREFIX=FitHiChIP_L10
+
+# Binary variable 1/0: if 1, overwrites any existing output file. otherwise (0), does not overwrite any output file.
+OverWrite=1
+EOT
+
+#####################################################################################################################
+
+# generate config file for S, 10kb
+configfile_S10="results/loops/fithichip/$sample_name/configfile_S10"
+touch $configfile_S10
+cat <<EOT >> $configfile_S10
+# File containing the valid pairs from HiCPro pipeline 
+ValidPairs=\${pairs_file}
+
+# File containing the bin intervals (according to a specified bin size)
+# which is an output of HiC-pro pipeline
+# If not provided, this is computed from the parameter 1
+Interval=
+
+# File storing the contact matrix (output of HiC-pro pipeline)
+# should be accompanied with the parameter 2
+# if not specified, computed from the parameter 1
+Matrix=
+
+# Pre-computed locus pair file
+# of the format: 
+# chr1 	start1 	end1 	chr2 	start2 	end2 	contactcounts
+Bed=
+
+# Boolean variable indicating if the reference genome is circular
+# by default, it is 0
+# if the genome is circular, the calculation of genomic distance is slightly different
+CircularGenome=0
+
+# File containing reference ChIP-seq / HiChIP peaks (in .bed format)
+# mandatory parameter
+PeakFile=\${peaks_file}
+
+# Output base directory under which all results will be stored
+OutDir=\${outdir_S10}
+
+#Interaction type - 1: peak to peak 2: peak to non peak 3: peak to all (default) 4: all to all 5: everything from 1 to 4.
+IntType=3
+
+# Size of the bins [default = 5000], in bases, for detecting the interactions.
+BINSIZE=10000
+
+# Lower distance threshold of interaction between two segments
+# (default = 20000 or 20 Kb)
+LowDistThr=20000
+
+# Upper distance threshold of interaction between two segments
+# (default = 2000000 or 2 Mb)
+UppDistThr=2000000
+
+# Applicable only for peak to all output interactions - values: 0 / 1
+# if 1, uses only peak to peak loops for background modeling - corresponds to FitHiChIP(S)
+# if 0, uses both peak to peak and peak to nonpeak loops for background modeling - corresponds to FitHiChIP(L)
+UseP2PBackgrnd=1
+
+# parameter signifying the type of bias vector - values: 1 / 2
+# 1: coverage bias regression	2: ICE bias regression
+BiasType=1
+
+# following parameter, if 1, means that merge filtering (corresponding to either FitHiChIP(L+M) or FitHiChIP(S+M))
+# depending on the background model, would be employed. Otherwise (if 0), no merge filtering is employed. Default: 1
+MergeInt=1
+
+# FDR (q-value) threshold for loop significance
+QVALUE=0.01
+
+# File containing chromomosome size values corresponding to the reference genome.
+ChrSizeFile=/mnt/BioAdHoc/Groups/vd-ay/Database_HiChIP_eQTL_GWAS/Data/RefGenome/chrsize/hg38.chrom.sizes
+
+# prefix string of all the output files (Default = 'FitHiChIP').
+PREFIX=FitHiChIP_S10
+
+# Binary variable 1/0: if 1, overwrites any existing output file. otherwise (0), does not overwrite any output file.
+OverWrite=1
+EOT
+
+#####################################################################################################################
+
+# generate config file for L, 25kb
+configfile_L25="results/loops/fithichip/$sample_name/configfile_L25"
+touch $configfile_L25
+cat <<EOT >> $configfile_L25
+# File containing the valid pairs from HiCPro pipeline 
+ValidPairs=\${pairs_file}
+
+# File containing the bin intervals (according to a specified bin size)
+# which is an output of HiC-pro pipeline
+# If not provided, this is computed from the parameter 1
+Interval=
+
+# File storing the contact matrix (output of HiC-pro pipeline)
+# should be accompanied with the parameter 2
+# if not specified, computed from the parameter 1
+Matrix=
+
+# Pre-computed locus pair file
+# of the format: 
+# chr1 	start1 	end1 	chr2 	start2 	end2 	contactcounts
+Bed=
+
+# Boolean variable indicating if the reference genome is circular
+# by default, it is 0
+# if the genome is circular, the calculation of genomic distance is slightly different
+CircularGenome=0
+
+# File containing reference ChIP-seq / HiChIP peaks (in .bed format)
+# mandatory parameter
+PeakFile=\${peaks_file}
+
+# Output base directory under which all results will be stored
+OutDir=\${outdir_L25}
+
+#Interaction type - 1: peak to peak 2: peak to non peak 3: peak to all (default) 4: all to all 5: everything from 1 to 4.
+IntType=3
+
+# Size of the bins [default = 5000], in bases, for detecting the interactions.
+BINSIZE=25000
+
+# Lower distance threshold of interaction between two segments
+# (default = 20000 or 20 Kb)
+LowDistThr=20000
+
+# Upper distance threshold of interaction between two segments
+# (default = 2000000 or 2 Mb)
+UppDistThr=2000000
+
+# Applicable only for peak to all output interactions - values: 0 / 1
+# if 1, uses only peak to peak loops for background modeling - corresponds to FitHiChIP(S)
+# if 0, uses both peak to peak and peak to nonpeak loops for background modeling - corresponds to FitHiChIP(L)
+UseP2PBackgrnd=0
+
+# parameter signifying the type of bias vector - values: 1 / 2
+# 1: coverage bias regression	2: ICE bias regression
+BiasType=1
+
+# following parameter, if 1, means that merge filtering (corresponding to either FitHiChIP(L+M) or FitHiChIP(S+M))
+# depending on the background model, would be employed. Otherwise (if 0), no merge filtering is employed. Default: 1
+MergeInt=1
+
+# FDR (q-value) threshold for loop significance
+QVALUE=0.01
+
+# File containing chromomosome size values corresponding to the reference genome.
+ChrSizeFile=/mnt/BioAdHoc/Groups/vd-ay/Database_HiChIP_eQTL_GWAS/Data/RefGenome/chrsize/hg38.chrom.sizes
+
+# prefix string of all the output files (Default = 'FitHiChIP').
+PREFIX=FitHiChIP_L25
+
+# Binary variable 1/0: if 1, overwrites any existing output file. otherwise (0), does not overwrite any output file.
+OverWrite=1
+EOT
+
+#####################################################################################################################
+
+# generate config file for S, 25kb
+configfile_S25="results/loops/fithichip/$sample_name/configfile_S25"
+touch $configfile_S25
+cat <<EOT >> $configfile_S25
+# File containing the valid pairs from HiCPro pipeline 
+ValidPairs=\${pairs_file}
+
+# File containing the bin intervals (according to a specified bin size)
+# which is an output of HiC-pro pipeline
+# If not provided, this is computed from the parameter 1
+Interval=
+
+# File storing the contact matrix (output of HiC-pro pipeline)
+# should be accompanied with the parameter 2
+# if not specified, computed from the parameter 1
+Matrix=
+
+# Pre-computed locus pair file
+# of the format: 
+# chr1 	start1 	end1 	chr2 	start2 	end2 	contactcounts
+Bed=
+
+# Boolean variable indicating if the reference genome is circular
+# by default, it is 0
+# if the genome is circular, the calculation of genomic distance is slightly different
+CircularGenome=0
+
+# File containing reference ChIP-seq / HiChIP peaks (in .bed format)
+# mandatory parameter
+PeakFile=\${peaks_file}
+
+# Output base directory under which all results will be stored
+OutDir=\${outdir_S25}
+
+#Interaction type - 1: peak to peak 2: peak to non peak 3: peak to all (default) 4: all to all 5: everything from 1 to 4.
+IntType=3
+
+# Size of the bins [default = 5000], in bases, for detecting the interactions.
+BINSIZE=25000
+
+# Lower distance threshold of interaction between two segments
+# (default = 20000 or 20 Kb)
+LowDistThr=20000
+
+# Upper distance threshold of interaction between two segments
+# (default = 2000000 or 2 Mb)
+UppDistThr=2000000
+
+# Applicable only for peak to all output interactions - values: 0 / 1
+# if 1, uses only peak to peak loops for background modeling - corresponds to FitHiChIP(S)
+# if 0, uses both peak to peak and peak to nonpeak loops for background modeling - corresponds to FitHiChIP(L)
+UseP2PBackgrnd=1
+
+# parameter signifying the type of bias vector - values: 1 / 2
+# 1: coverage bias regression	2: ICE bias regression
+BiasType=1
+
+# following parameter, if 1, means that merge filtering (corresponding to either FitHiChIP(L+M) or FitHiChIP(S+M))
+# depending on the background model, would be employed. Otherwise (if 0), no merge filtering is employed. Default: 1
+MergeInt=1
+
+# FDR (q-value) threshold for loop significance
+QVALUE=0.01
+
+# File containing chromomosome size values corresponding to the reference genome.
+ChrSizeFile=/mnt/BioAdHoc/Groups/vd-ay/Database_HiChIP_eQTL_GWAS/Data/RefGenome/chrsize/hg38.chrom.sizes
+
+# prefix string of all the output files (Default = 'FitHiChIP').
+PREFIX=FitHiChIP_S25
+
+# Binary variable 1/0: if 1, overwrites any existing output file. otherwise (0), does not overwrite any output file.
+OverWrite=1
+EOT
+
+###########################################################################################################################
+
+# run fithichip
+$fithichip_call_loops -C $configfile_L5
+$fithichip_call_loops -C $configfile_S5
+$fithichip_call_loops -C $configfile_L10
+$fithichip_call_loops -C $configfile_S10
+$fithichip_call_loops -C $configfile_l25
+$fithichip_call_loops -C $configfile_S25
 
 # print end message
 echo "Ended: fithichip loop calling"
