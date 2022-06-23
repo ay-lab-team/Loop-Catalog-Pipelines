bee6a63e95a3a50c5e9a45c44441fe12539dfe16
Kyra Fetter <kfetter@herman-login1.hpc.lji.org>
2022-06-23 14:39:31 -0700

    update digest_reference_genome.sh

--- workflow/scripts/refs/digest_reference_genome.sh
+++ workflow/scripts/refs/digest_reference_genome.sh
@@ -1,16 +1,21 @@
+#PBS -l nodes=1:ppn=1
+#PBS -l mem=30gb
+#PBS -l walltime=20:00:00
+#PBS -e results/refs/logs/
+#PBS -o results/refs/logs/
+#PBS -N digest_reference_genome
+#PBS -V
+
+# example run:
+# 1) qsub -F "<name of RE (lowercase)> <restriction site>" digest_reference_genome.sh
+
 # path of files
-output="results/refs/restriction_enzyme/hg38.hicpro.arima.txt"
+output="/mnt/BioAdHoc/Groups/vd-ay/Database_HiChIP_eQTL_GWAS/Data/RefGenome/Restriction_Fragment/hg38/hg38_$1_digestion.bed"
 ref_fasta="results/refs/reference_genomes/RefGenome/fasta/hg38/hg38.fa"
 
 # path of softwares 
 source config/config.sh
-
-# make the output directory 
-outdir=$(dirname $output)
-mkdir -p $outdir
+source workflow/source_paths.sh
 
 # digest the genome
-python $hicpro_digest \
-            -r ^GATC G^ANTC \
-            -o $output \
-            $ref_fasta
+$hicpro_python $hicpro_digest -r $2 -o $output $ref_fasta
