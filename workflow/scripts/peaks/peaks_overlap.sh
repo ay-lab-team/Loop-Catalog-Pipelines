f5c04490340c0a773ba5ef163239002278cf647a
Kyra Fetter <kfetter@herman-login1.hpc.lji.org>
2023-05-05 10:16:42 -0700

    add peaks overlap script

--- workflow/scripts/peaks/peaks_overlap.sh
+++ workflow/scripts/peaks/peaks_overlap.sh
@@ -1,6 +1,6 @@
 #PBS -l nodes=1:ppn=1
-#PBS -l mem=5gb
-#PBS -l walltime=1:00:00
+#PBS -l mem=1gb
+#PBS -l walltime=0:10:00
 #PBS -e results/peaks/logs/
 #PBS -o results/peaks/logs/
 #PBS -N peaks_overlap.sh
@@ -36,50 +36,61 @@ echo "sample_name: $sample_name"
 echo
 
 # identify all peaks files for one sample, then bgzip and tabix
-file_samplesheet="results/samplesheets/post-hicpro/peaks_files.samplesheet.without_header.tsv"
+file_samplesheet="results/samplesheets/post-hicpro/human_updated_0314.peaks_files.samplesheet.without_header.tsv"
 unset IFS
 sample_info=( $(grep "${sample_name}" ${file_samplesheet}) )
+
+genome="hg38"
+chr_sizes="/mnt/BioAdHoc/Groups/vd-ay/Database_HiChIP_eQTL_GWAS/Data/RefGenome/chrsize/${genome}.chrom.sizes"
 
 # call overlaps if necessary peak files present
 if [ -f "${sample_info[4]}" ]; then
 
     cs_file=${sample_info[4]}
-    cs_file_s="results/peaks/overlaps/no_slop_recall/${sample_name}.cs.bed"
-    cat $cs_file | sort -k1,1 -k2,2n > $cs_file_s
+    cs_file_s="results/peaks/overlaps/04_25_2023/${sample_name}.chipseq.$genome.bed"
+    awk -F["\t"] '{print $1"\t"$2"\t"$3}' $cs_file | sort -k1,1 -k2,2n -k3,3n -u > $cs_file_s
 
     if [ -f "${sample_info[2]}" ]; then
 
         hp_file=${sample_info[2]}
-        hp_file_s="results/peaks/overlaps/no_slop_recall/${sample_name}.hp.bed"
-        outfile="results/peaks/overlaps/no_slop_recall/${sample_name}.overlaps.chipseq.hp.txt"
-        outfile_no_dups="results/peaks/overlaps/no_slop_recall/${sample_name}.overlaps.chipseq.hp.nodups.txt"
-        cat $hp_file | sort -k1,1 -k2,2n > $hp_file_s
+        hp_file_s="results/peaks/overlaps/04_25_2023/${sample_name}.hp.sorted.bed"
+        hp_file_slop="results/peaks/overlaps/04_25_2023/${sample_name}.hp.sorted.slop.bed"
+        outfile="results/peaks/overlaps/04_25_2023/${sample_name}.overlaps.chipseq.hichip_peaks.$genome.txt"
+        outfile_no_dups="results/peaks/overlaps/04_25_2023/${sample_name}.chipseq.hichip_peaks.uniq.$genome.txt"
+        awk -F["\t"] 'substr($1,1,1) ~ /c/ {print $1"\t"$2"\t"$3}' $hp_file | sort -k1,1 -k2,2n -k3,3n -u > $hp_file_s
 
-        bedtools intersect -wa -a $cs_file_s -b $hp_file_s -sorted > $outfile
+        bedtools slop -i $hp_file_s -g $chr_sizes -b 1000 > $hp_file_slop
+
+        bedtools intersect -wa -a $cs_file_s -b $hp_file_slop -sorted > $outfile
 
         cat $outfile | uniq > $outfile_no_dups
 
-        rm $hp_file_s 
+        rm $hp_file_s
+        rm $hp_file_slop
+        rm $outfile
     
     fi
 
     if [ -f "${sample_info[3]}" ]; then
 
         f_file=${sample_info[3]}
-        f_file_s="results/peaks/overlaps/no_slop_recall/${sample_name}.f.bed"
-        outfile="results/peaks/overlaps/no_slop_recall/${sample_name}.overlaps.chipseq.f.txt"
-        outfile_no_dups="results/peaks/overlaps/no_slop_recall/${sample_name}.overlaps.chipseq.f.nodups.txt"
-        cat $f_file | sort -k1,1 -k2,2n > $f_file_s
+        f_file_s="results/peaks/overlaps/04_25_2023/${sample_name}.f.sorted.bed"
+        f_file_slop="results/peaks/overlaps/04_25_2023/${sample_name}.f.sorted.slop.bed"
+        outfile="results/peaks/overlaps/04_25_2023/${sample_name}.overlaps.chipseq.fithichip.$genome.txt"
+        outfile_no_dups="results/peaks/overlaps/04_25_2023/${sample_name}.chipseq.fithichip.uniq.$genome.txt"
+        awk -F["\t"] 'substr($1,1,1) ~ /c/ {print $1"\t"$2"\t"$3}' $f_file | sort -k1,1 -k2,2n -k3,3n -u > $f_file_s
 
-        bedtools intersect -wa -a $cs_file_s -b $f_file_s -sorted > $outfile
+        bedtools slop -i $f_file_s -g $chr_sizes -b 1000 > $f_file_slop
+
+        bedtools intersect -wa -a $cs_file_s -b $f_file_slop -sorted > $outfile
 
         cat $outfile | uniq > $outfile_no_dups
 
         rm $f_file_s
+        rm $f_file_slop
+        rm $outfile
 
     fi
-
-    rm $cs_file_s
     
 else
     echo "missing chipseq peaks file"
