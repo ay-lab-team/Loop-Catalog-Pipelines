d8b599ec3332e6e1de431f36e1dc6d3e93b0ebe4
joreynajr <joreyna@live.com>
2023-10-03 09:18:18 -0700

    Clarified the extracting of problem samples

--- workflow/scripts/rename_samples/get_old_samples.sh
+++ workflow/scripts/rename_samples/get_old_samples.sh
@@ -11,6 +11,7 @@ find results/loops/fithichip/* -maxdepth 0 -type d >> $cand_paths_fn
 find results/loops/hiccups/whole_genome/* -maxdepth 0 -type d >> $cand_paths_fn
 find results/qc/fastqc/* -maxdepth 0 -type d >> $cand_paths_fn
 
+# locate the final problematic paths
 problem_samples_fn="results/misc/rename_samples/copy_and_rename.samples.txt"
 problem_paths_fn="results/misc/rename_samples/problem_paths.txt"
 grep -f $problem_samples_fn $cand_paths_fn > $problem_paths_fn
