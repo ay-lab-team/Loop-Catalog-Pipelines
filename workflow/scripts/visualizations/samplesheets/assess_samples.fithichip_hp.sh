aa741e1df02e379378034b8c104740889f6790cd
joreynajr <joreyna@live.com>
2023-10-10 12:05:07 -0700

    Pushing visualization scripts

--- workflow/scripts/visualizations/samplesheets/assess_samples.fithichip_hp.sh
+++ workflow/scripts/visualizations/samplesheets/assess_samples.fithichip_hp.sh
@@ -1,12 +1,16 @@
+
+# set input and output paths
 samplesheet="workflow/scripts/visualizations/samplesheets/samplesheet.fithichip_hp.txt"
 wc_output="workflow/scripts/visualizations/samplesheets/samplesheet.fithichip_hp.wc.txt"
+
+# remove output if already exists
 if [[ -f $wc_output ]]; then
   rm $wc_output
 else
   touch $wc_output
 fi
 
-# count the number of lines in each file
+# count the number of lines in each loop file
 for fn in $(cat $samplesheet); do
   sample_wc=$(wc -l $fn |awk '{print $1}')
   sample_wc=$(expr $sample_wc - 1)
@@ -14,5 +18,7 @@ for fn in $(cat $samplesheet); do
   then
     sample_wc=0
   fi
+
+  # write the count into the file
   echo -e "$fn\t$sample_wc" >> $wc_output
 done
