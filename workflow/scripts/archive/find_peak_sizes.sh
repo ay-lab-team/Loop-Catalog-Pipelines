27566aba102d6ae5673deabb91b6f3627c9dd69b
Kyra Fetter <kfetter@login02.herman.lji.org>
2025-05-02 13:13:08 -0700

    clean trackers

--- workflow/scripts/archive/find_peak_sizes.sh
+++
@@ -1,4 +0,0 @@
-for sample in lee2021-gse179545-rna_pol_ii-hct116-wt-dpnii-rep1 lee2021-gse179545-rna_pol_ii-hct116-wt-dpnii-rep2 lee2021-gse179545-rna_pol_ii-hct116-auxin-dpnii-rep1 lee2021-gse179545-rna_pol_ii-hct116-auxin-dpnii-rep2
-do
-    awk -F["\t"] '{if (NR>0) {print $1"\t"$2"\t"$3"\t"($3-$2)}}' ${sample}/MACS2_ExtSize/out_macs2_peaks.narrowPeak > out_peak_sizes_${sample}.txt 
-done
